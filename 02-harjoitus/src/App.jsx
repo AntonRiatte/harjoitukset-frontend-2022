4cf69c85c1307d8e5591dd2812cf6eae09c472fe
Juho S <juho.salli@gmail.com>
2022-11-07 09:26:24 +0200

    02: poistaa turhat rivit app.jsx

--- 02-harjoitus/src/App.jsx
+++ 02-harjoitus/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import MainComponent from "./MainComponent";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <Header />
